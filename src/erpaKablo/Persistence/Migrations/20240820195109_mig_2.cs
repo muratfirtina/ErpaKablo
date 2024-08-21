89d2434f9b48cc9136806725fc04fa239526fe2d
muratfirtina <muratfirtina@hotmail.com>
2024-08-21 15:25:23 +0300

    newmigs

---
+++ src/erpaKablo/Persistence/Migrations/20240820195109_mig_2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Products",
+                type: "longtext",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Categories",
+                type: "longtext",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Categories");
+        }
+    }
+}
